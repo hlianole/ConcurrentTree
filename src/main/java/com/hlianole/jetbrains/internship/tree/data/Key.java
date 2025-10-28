366239bb6bcd0a6b677c7c3e4a5a7de55b3cef05
Oleksandr Hliantsev <hlianole@cvut.cz>
2025-10-28 16:28:54 +0100

    Implement own byte[] comparison

--- src/main/java/com/hlianole/jetbrains/internship/tree/data/Key.java
+++ src/main/java/com/hlianole/jetbrains/internship/tree/data/Key.java
@@ -2,13 +2,28 @@ package com.hlianole.jetbrains.internship.tree.data;
 
 public class Key {
 
-    private final String str;
+    private final byte[] str;
 
     public Key(byte[] key) {
-        this.str = new String(key);
+        this.str = key;
     }
 
-    public int compare(Key key) {
-        return str.compareTo(key.str);
+    public int compare(Key other) {
+        byte[] me = this.str;
+        byte[] oth = other.str;
+        int len = min(me.length, oth.length);
+
+        for (int i = 0; i < len; i++) {
+            int cmp = (me[i] & 0xff) - (oth[i] & 0xff);
+            if (cmp != 0) {
+                return cmp;
+            }
+        }
+
+        return me.length - oth.length;
+    }
+
+    private int min(int a, int b) {
+        return a < b ? a : b;
     }
 }
